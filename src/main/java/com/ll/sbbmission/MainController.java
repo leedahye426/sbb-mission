9a4b3e8c19d9d70bada64e227d92d4668fbc13f5
leedahye426 <9535957@naver.com>
2023-11-27 14:13:42 +0900

    2_16

--- src/main/java/com/ll/sbbmission/MainController.java
+++ src/main/java/com/ll/sbbmission/MainController.java
@@ -2,13 +2,11 @@ package com.ll.sbbmission;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MainController {
 
     @GetMapping("/")
-    @ResponseBody
     public String root() {
         return "redirect:/question/list";
     }
