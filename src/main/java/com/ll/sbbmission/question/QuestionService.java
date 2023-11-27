24cfc57ad6ee365a73306827b4a0c736f5349ec8
leedahye426 <9535957@naver.com>
2023-11-27 10:15:30 +0900

    2_9

---
+++ src/main/java/com/ll/sbbmission/question/QuestionService.java
@@ -0,0 +1,17 @@
+package com.ll.sbbmission.question;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class QuestionService {
+
+    private final QuestionRepository questionRepository;
+
+    public List<Question> getList() {
+        return questionRepository.findAll();
+    }
+}
