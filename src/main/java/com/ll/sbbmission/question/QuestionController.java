7ce64941f1c47a462cec85d0fca97476ad7f746f
leedahye426 <9535957@naver.com>
2023-11-27 10:10:11 +0900

    2_7

---
+++ src/main/java/com/ll/sbbmission/question/QuestionController.java
@@ -0,0 +1,23 @@
+package com.ll.sbbmission.question;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/question")
+public class QuestionController {
+    private final QuestionRepository questionRepository;
+
+    @GetMapping("/list")
+    public String list(Model model) {
+        List<Question> questionList =  questionRepository.findAll();
+        model.addAttribute("questionList", questionList);
+        return "question_list";
+    }
+}
